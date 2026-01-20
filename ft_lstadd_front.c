a78d867c9d970510670fc9f7552da8369545f1ba
Ada Guney Ak <adak@k1m23s01.42istanbul.com.tr>
2026-01-20 18:59:18 +0300

    build(project/libft):adding linked lists

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: adak <adak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 17:53:58 by adak              #+#    #+#             */
-/*   Updated: 2026/01/20 18:16:22 by adak             ###   ########.fr       */
+/*   Updated: 2026/01/20 18:57:57 by adak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,21 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	t_list	*temp;
 	temp = new;
-	temp->next = lst;
-	lst = temp;
+	temp->next = *lst;
+	lst = &temp;
+}
+
+#include <stdio.h>
+
+int main(void) {
+
+	int a = 1;
+	int b = 2;
+
+	t_list *list = ft_lstnew(&a);
+	t_list *new = ft_lstnew(&b);
+	
+
+	ft_lstadd_front(&list, new);
+	printf("%d\n", *(unsigned char *)(list->content));
 }
