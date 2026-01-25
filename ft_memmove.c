9449c2e821716208007341021ff3b905ffd8e872
Ada Guney Ak <adak@k1m18s09.42istanbul.com.tr>
2026-01-25 18:17:52 +0300

    build(project/libft):more controls

--- ft_memmove.c
+++ ft_memmove.c
@@ -18,25 +18,20 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*s;
 	size_t			i;
 
+	if (!dest && !src)
+		return (NULL);
 	d = (unsigned char *) dest;
 	s = (unsigned char *) src;
 	i = 0;
 	if (d > s)
-	{
-		while (n > 0)
-		{
-			n--;
+		while (n-- > 0)
 			d[n] = s[n];
-		}
-	}
 	else
-	{
 		while (i < n)
 		{
 			d[i] = s[i];
 			i++;
 		}
-	}
 	return (dest);
 }
 
